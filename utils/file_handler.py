f0ce382ebe05e1683f1431d38311ecdb34e3390f
frewily <2994508531@qq.com>
2026-05-05 17:10:42 +0800

    向量存储服务开发

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -35,7 +35,7 @@ def listdir_with_allowed_type(path: str, allowed_types: tuple[str]):  #返回文
 
     if not os.path.exists(path):
         logger.error(f"Path {path} does not exist")
-        return allowed_types
+        return []
 
     for f in os.listdir(path):
         if f.endswith(allowed_types):
